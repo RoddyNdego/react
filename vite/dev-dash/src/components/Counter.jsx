a0a46e612fd3593090455e0ad9dfecfa45ede208
Anorme <anormeinkumsah@gmail.com>
2025-06-10 02:21:06 +0000

    Remove inline styles from component

--- vite/dev-dash/src/components/Counter.jsx
+++ vite/dev-dash/src/components/Counter.jsx
@@ -1,14 +1,15 @@
-import "./counter-styles.css"
-import { useState } from "react"
+import { useState } from 'react';
+
+import './counter-styles.css'
 
 function Counter() {
    const[count, setCount] = useState(0)
     return(
         <div>
             <h1>Counter: {count}</h1>
-            <button style={{backgroundColor: 'gray' ,marginRight: '10px'}} className="btn_1" onClick={() => setCount(count + 1)}>Increment</button>
-            <button style={{backgroundColor: 'gray' ,marginRight: '10px'}} className="btn_2" onClick={() => setCount(count - 1)}>Decrement</button>
-            <button style={{backgroundColor: 'gray' ,marginRight: '10px'}} className="btn_3" onClick={() => setCount(0)}>Reset</button> 
+            <button className="btn_1" onClick={() => setCount(count + 1)}>Increment</button>
+            <button className="btn_2" onClick={() => setCount(count - 1)}>Decrement</button>
+            <button className="btn_3" onClick={() => setCount(0)}>Reset</button> 
         </div>
     )
 }
