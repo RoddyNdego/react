1579f8411eb1793d868e26ed5c2b313c2b9b70a7
Anorme <anormeinkumsah@gmail.com>
2025-06-10 02:18:18 +0000

    Comment out unsused code

--- vite/dev-dash/src/App.jsx
+++ vite/dev-dash/src/App.jsx
@@ -1,33 +1,30 @@
 
 import Counter from "./components/Counter"
-import { useState } from "react"
+// import { useState } from "react"
 import './App.css'
 
 function App() {
 
   return <Counter/>
 
+  // const [popupOpen, setPopupOpen] = useState(false)
 
-
-  const [popupOpen, setPopupOpen] = useState(false)
-
-
-  return (
-    <>
-      <Title todo_title="To-do list" />
-        <div>
-          <input type="text" onChange={(event) =>{
-            console.log(event.target.value)
-          }}/>
-          <button onClick={() => setPopupOpen(true)}>Add to do</button>
-        </div>
-      <Todo task="Get ready for tomorrow"/>
-      <Todo task="Go to bed early"/>
-      <Todo task="Wake up early"/>
-      <Todo task="Go to work"/>
-      { popupOpen && <Popup alert="Are you reallyyy sure?"/>}
-    </>
-  )
+  // return (
+  //   <>
+  //     <Title todo_title="To-do list" />
+  //       <div>
+  //         <input type="text" onChange={(event) =>{
+  //           console.log(event.target.value)
+  //         }}/>
+  //         <button onClick={() => setPopupOpen(true)}>Add to do</button>
+  //       </div>
+  //     <Todo task="Get ready for tomorrow"/>
+  //     <Todo task="Go to bed early"/>
+  //     <Todo task="Wake up early"/>
+  //     <Todo task="Go to work"/>
+  //     { popupOpen && <Popup alert="Are you reallyyy sure?"/>}
+  //   </>
+  // )
 }
 
 export default App
